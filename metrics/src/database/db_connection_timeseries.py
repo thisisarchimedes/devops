e70096d5c80999a23ca39eece3576e89d63d3730
tomerXarchimedes <tomer@archimedesfi.com>
2024-01-06 13:17:47 -0500

    Update db_connection_timeseries.py

--- metrics/src/database/db_connection_timeseries.py
+++ metrics/src/database/db_connection_timeseries.py
@@ -32,78 +32,74 @@ class DBConnectionTimeseries(DBConnection):
             print(f"Error querying database: {e}")
             return []
 
-    def get_days_per_week_with_deploy(self, repos_name: List[str]) -> pd.DataFrame:
-        client = self.create_timestream_client('timestream-query')
-
-        # Convert the list of repository names to a format suitable for the SQL query
-        repo_names_str = ', '.join(f"'{name}'" for name in repos_name)
-
-        # Modify the query to include the list of repository names
-        query = f"""
-        WITH DailyDeployCount AS (
-            SELECT date_trunc('day', time) AS day, COUNT(*) AS deploy_count
-            FROM "{self.database_name}"."{self.table_name}"
-            WHERE "event" = 'deploy'
-                AND "repo_name" IN ({repo_names_str})
-                AND time >= date_add('month', -3, current_date)
-            GROUP BY date_trunc('day', time)
-        ),
-        DaysWithDeployPerWeek AS (
-            SELECT date_trunc('week', day) AS week, COUNT(*) AS days_with_deploy
-            FROM DailyDeployCount
-            WHERE deploy_count > 0
-            GROUP BY date_trunc('week', day)
-        )
-        SELECT week, days_with_deploy
-        FROM DaysWithDeployPerWeek
-        ORDER BY week ASC
-        """
-
+    def get_days_per_week_with_deploy(self, repos: List[str]) -> pd.DataFrame:
+        query = self.build_deploy_query(repos)
         try:
-            response = client.query(QueryString=query)
-            query_results_df = self.parse_days_per_week_response(response)
-            print(query_results_df)
-            res =  self.fill_missing_weeks(query_results_df)
-            print(res)
-            return res
+            query_result = self.execute_query(query)
+            return self.process_deploy_data(query_result)
         except Exception as e:
             print(f"Error querying database: {e}")
             return pd.DataFrame()
-
-    def fill_missing_weeks(self, query_result_df: pd.DataFrame) -> pd.DataFrame:
-        # Normalize week column to the start of the week (e.g., Monday at 00:00:00)
-        query_result_df['week'] = pd.to_datetime(query_result_df['week']).dt.to_period('W-MON').dt.start_time
-
-        # Generate a list of all weeks in the last 3 months
-        end_date = datetime.now()
-        start_date = end_date - timedelta(weeks=12)
-        all_weeks = pd.date_range(start=start_date, end=end_date, freq='W-MON').to_frame(index=False, name='week')
-
-        # Normalize all_weeks to the start of the week
-        all_weeks['week'] = all_weeks['week'].dt.to_period('W-MON').dt.start_time
-
-        # Merge with query results to find missing weeks
-        final_results = all_weeks.merge(query_result_df, on='week', how='left')
-        final_results['days_with_deploy'].fillna(0, inplace=True)
-
-        return final_results
-
-    
-
-    def parse_days_per_week_response(self, response) -> pd.DataFrame:
-        # Extract data into a DataFrame
-        data = []
-        for row in response['Rows']:
-            week = row['Data'][0]['ScalarValue']
-            days_with_deploy = int(row['Data'][1]['ScalarValue'])
-            data.append({'week': week, 'days_with_deploy': days_with_deploy})
-
-        return pd.DataFrame(data)
-
+        
     
     def create_timestream_client(self, client_type: str):
         return boto3.client(client_type)
+
+
+    def build_deploy_query(self, repos: List[str]) -> str:
+        repo_names_str = self.format_repo_names(repos)
+        return f"""
+            WITH DailyDeployCount AS (
+                SELECT date_trunc('day', time) AS day, COUNT(*) AS deploy_count
+                FROM "{self.database_name}"."{self.table_name}"
+                WHERE "event" = 'deploy' AND "repo_name" IN ({repo_names_str})
+                AND time >= date_add('month', -3, current_date)
+                GROUP BY date_trunc('day', time)
+            ),
+            DaysWithDeployPerWeek AS (
+                SELECT date_trunc('week', day) AS week, COUNT(*) AS days_with_deploy
+                FROM DailyDeployCount WHERE deploy_count > 0
+                GROUP BY date_trunc('week', day)
+            )
+            SELECT week, days_with_deploy FROM DaysWithDeployPerWeek ORDER BY week ASC
+            """
+
+    def format_repo_names(self, repos: List[str]) -> str:
+        return ', '.join(f"'{repo}'" for repo in repos)
+
+    def execute_query(self, query: str):
+        client = self.create_timestream_client('timestream-query')
+        return client.query(QueryString=query)
+
+    def process_deploy_data(self, query_response):
+        deploy_data = self.parse_query_response(query_response)
+        return self.fill_missing_weeks(deploy_data)
+
+    def fill_missing_weeks(self, deploy_data: pd.DataFrame) -> pd.DataFrame:
+        deploy_data['week'] = self.normalize_weeks(deploy_data['week'])
+        all_weeks = self.generate_week_series()
+        return self.merge_week_series_with_data(all_weeks, deploy_data)
+
+    def normalize_weeks(self, week_series: pd.Series) -> pd.Series:
+        return pd.to_datetime(week_series).dt.to_period('W-MON').dt.start_time
+
+    def generate_week_series(self) -> pd.DataFrame:
+        start_date = datetime.now() - timedelta(weeks=12)
+        end_date = datetime.now()
+        return pd.date_range(start=start_date, end=end_date, freq='W-MON').to_frame(index=False, name='week')
+
+    def merge_week_series_with_data(self, week_series: pd.DataFrame, data: pd.DataFrame) -> pd.DataFrame:
+        merged_data = week_series.merge(data, on='week', how='left')
+        merged_data['days_with_deploy'].fillna(0, inplace=True)
+        return merged_data
     
+
+    def parse_query_response(self, response) -> pd.DataFrame:
+        data = [{'week': row['Data'][0]['ScalarValue'], 
+                'days_with_deploy': int(row['Data'][1]['ScalarValue'])}
+                for row in response['Rows']]
+        return pd.DataFrame(data)
+
 
     def prepare_record(self, repo_name, event, metadata) -> dict:
 
