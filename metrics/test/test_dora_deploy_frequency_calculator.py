f63cbfe720ccdfab8e867957d91a327017732a84
tomerXarchimedes <tomer@archimedesfi.com>
2024-01-08 16:54:58 -0500

    Update test_dora_deploy_frequency_calculator.py

--- metrics/test/test_dora_deploy_frequency_calculator.py
+++ metrics/test/test_dora_deploy_frequency_calculator.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 from src.calculations.dora_deploy_frequency_calculator import DORADeployFrequencyCalculator
 
-
 class TestDORADeployFrequencyCalculator:
 
     def setup_method(self) -> None:
