eabf518c365771ae94b0e53f2afb06fe73c1696f
tomerXarchimedes <tomer@archimedesfi.com>
2024-01-08 15:58:23 -0500

    Update dora_deploy_frequency_calculator.py

--- metrics/src/calculations/dora_deploy_frequency_calculator.py
+++ metrics/src/calculations/dora_deploy_frequency_calculator.py
@@ -14,11 +14,11 @@ class DORADeployFrequencyCalculator():
         """
 
         daily_deploy_volume = self._prepare_daily_volume_data(daily_deploy_volume)
-        start_date, end_date = self.adjust_date_range(start_date, end_date)
-        date_range = self.create_weekly_date_range(start_date, end_date)
+        start_date, end_date = self._adjust_date_range(start_date, end_date)
+        date_range = self._create_weekly_date_range(start_date, end_date)
         
-        weekly_deploy_count = self.calculate_weekly_deploy_counts(daily_deploy_volume)
-        return self.create_output_dataframe(date_range, weekly_deploy_count)
+        weekly_deploy_count = self._calculate_weekly_deploy_counts(daily_deploy_volume)
+        return self._create_output_dataframe(date_range, weekly_deploy_count)
 
     def _prepare_daily_volume_data(self, daily_deploy_volume: pd.DataFrame) -> pd.DataFrame:
         """Filter out weekends and convert non-zero deploy counts to 1."""
@@ -30,37 +30,37 @@ class DORADeployFrequencyCalculator():
 
         return daily_deploy_volume
 
-    def adjust_date_range(self, start_date: date, end_date: date) -> tuple:
+    def _adjust_date_range(self, start_date: date, end_date: date) -> tuple:
         """Adjust start and end dates to the nearest Monday and Sunday, respectively."""
 
-        start_date = self.adjust_to_previous_monday(start_date)
-        end_date = self.adjust_to_following_sunday(end_date)
+        start_date = self._adjust_to_previous_monday(start_date)
+        end_date = self._adjust_to_following_sunday(end_date)
 
         return start_date, end_date
 
-    def adjust_to_previous_monday(self, some_date: date) -> datetime:
+    def _adjust_to_previous_monday(self, some_date: date) -> datetime:
         """Adjust a date to the previous Monday."""
 
         return datetime.combine(some_date, time.min) - timedelta(days=some_date.weekday())
 
-    def adjust_to_following_sunday(self, some_date: date) -> datetime:
+    def _adjust_to_following_sunday(self, some_date: date) -> datetime:
         """Adjust a date to the following Sunday."""
 
         adjustment = timedelta(days=6 - some_date.weekday()) 
         return datetime.combine(some_date, time.min) + adjustment
 
-    def create_weekly_date_range(self, start_date: date, end_date: date) -> pd.DatetimeIndex:
+    def _create_weekly_date_range(self, start_date: date, end_date: date) -> pd.DatetimeIndex:
         """Create a weekly date range starting from Monday."""
 
         return pd.date_range(start=start_date, end=end_date, freq='W-MON')
 
-    def calculate_weekly_deploy_counts(self, daily_deploy_volume: pd.DataFrame) -> pd.Series:
+    def _calculate_weekly_deploy_counts(self, daily_deploy_volume: pd.DataFrame) -> pd.Series:
         """Calculate the sum of deployment counts for each week."""
 
         daily_deploy_volume['WeekStart'] = daily_deploy_volume['Day'].apply(lambda x: x - timedelta(days=x.weekday()))
         return daily_deploy_volume.groupby('WeekStart')['DeployCount'].sum()
 
-    def create_output_dataframe(self, date_range, weekly_deploy_counts: pd.Series) -> pd.DataFrame():
+    def _create_output_dataframe(self, date_range, weekly_deploy_counts: pd.Series) -> pd.DataFrame():
         """Create the output DataFrame with weeks and deployment counts."""
 
         output = pd.DataFrame({'Week': date_range})
