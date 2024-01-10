bc8d3ec78dcf56e5867224ecd1a7ae6b31c5f09d
tomerXarchimedes <tomer@archimedesfi.com>
2024-01-09 22:36:19 -0500

    Update event_logger.py

--- metrics/src/event_processor/logger/event_logger.py
+++ metrics/src/event_processor/logger/event_logger.py
@@ -9,7 +9,7 @@ DEVOPS_METRICS_SERVICE_LABEL = 'devops-metrics'
 @dataclass
 class EventLogItem():
 
-    event: str
+    event: str = ''
     message: str = ''
     service: str = DEVOPS_METRICS_SERVICE_LABEL
 
@@ -20,8 +20,12 @@ class EventLogger():
         pass
 
     def get_event_log_item_from_df_event(self, event: pd.DataFrame) -> EventLogItem:
+
+        # DF always has one row
+        event_item_json = event.iloc[0].to_json(orient='index')
+
         return EventLogItem(
-            event=event.to_json(orient='records'),
+            event=event_item_json,
             message=f'Repo: {event["Repo"].iloc[0]} ; Event: {event["Event"].iloc[0]}',
             service=DEVOPS_METRICS_SERVICE_LABEL,
         )
