a45fbfb9ad5516f7f9c93839ef357d793b22d5c8
tomerXarchimedes <tomer@archimedesfi.com>
2024-01-11 09:50:35 -0500

    adding Lambda entry

--- metrics/src/event_processor/entry_lambda.py
+++ metrics/src/event_processor/entry_lambda.py
@@ -1,63 +1,51 @@
 import json
-import os
 
-from src.event_processor.simple_authenticator import SimpleAuthenticator
-from src.event_processor.payload_validator import PayloadValidator
-from src.event_processor.database.db_connection_timeseries import DBConnectionTimeseries
-from src.event_processor.logger.event_logger_new_relic import EventLoggerNewRelic
-from src.event_processor.events.factory_event import FactoryEvent
+from src.event_processor.config.simple_authenticator import SimpleAuthenticator
+from src.event_processor.config.config_aws import ConfigAWS
+from src.event_processor.process_new_event import process_new_event
 
-def lambda_handler(event, context):
+def entry_point_lambda(raw_event, context):
     
     try:
-        client_auth_token = _get_auth_token(event)    
-        event_payload = _get_event_payload(event)
+        headers = _get_headers(raw_event)
+        body = _get_body(raw_event)
+
+        config = ConfigAWS()
+        _authenticate(headers, config)
+
     except Exception as e:
         print(f'Error initializing Lambda: {e}')
         return _get_response()
     
     try:
-        preprocess_validation(client_auth_token, event_payload)
-        process_new_event(event_payload)
+        process_new_event(body, config)
     except Exception as e:
         print(f'Error processing event: {e}')
 
     return _get_response()
 
 
-def process_new_event(event_payload: dict) -> None:
+def _authenticate(headers, config: ConfigAWS):
     
-    database_name = os.getenv('DEVOPS_DB_NAME')
-    table_name = os.getenv('DEVOPS_TABLE_NAME')
-
-    db_connection = DBConnectionTimeseries(database_name, table_name)
-
-    event_logger = EventLoggerNewRelic()
-
-    factory_event = FactoryEvent(db_connection, event_logger)
-    event = factory_event.create_event(event_payload)
-    event.process()
-    
-
-def preprocess_validation(event_payload: dict, client_auth_token: str) -> None:
-
-    expected_auth_token = os.getenv('SECRET_TOKEN', None)
+    client_auth_token = _get_auth_token(headers)
+    expected_auth_token = config.get_expected_auth_token()
     if SimpleAuthenticator(expected_auth_token).is_autherized(client_auth_token) == False:
-        raise Exception('Unauthorized request')
-    
-    if PayloadValidator().is_payload_valid(event_payload) == False:
-        raise Exception('Invalid payload')
-    
+            raise Exception('Error: Unauthorized request')
+        
 
-def _get_auth_token(event) -> str:
+def _get_auth_token(headers) -> str:
 
-    headers = event.get('headers', {})
     auth_token = headers.get('X-Secret-Token')
     return auth_token
-    
-def _get_event_payload(event) -> dict:
-    payload = json.loads(event.get('body', '{}'))
+
+def _get_body(event) -> dict:
+    payload = json.loads(event)['body']
     return payload
+    
+def _get_headers(event) -> dict:
+    payload = json.loads(event)['headers']
+    return payload
+
     
 def _get_response():
      # Always return 200 success for security reasons
---
+++ metrics/test/integration/test_integration_lambda_process_event.py
@@ -0,0 +1,56 @@
+import pytest
+import json
+from datetime import datetime
+
+from src.event_processor.config.config_aws import ConfigAWS
+from src.event_processor.entry_lambda import entry_point_lambda
+
+class TestIntegrationLambdaProcessEvent():
+
+    def test_process_event_deploy(self, capfd):
+        
+        auth_token = ConfigAWS().get_expected_auth_token()
+
+        devops_event_payload = {
+            'Time': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            'Repo': 'test_repo',
+            'Event': 'deploy',
+            'Metadata': 'LAMBDA'
+        }
+        
+        aws_event = {
+                        "httpMethod": "POST",
+                        "body": devops_event_payload,
+                        "headers": {
+                            "Content-Type": "application/json",
+                            "X-Secret-Token": auth_token
+                        },
+  
+                    }
+
+        aws_context = {
+                        "function_name": "my_lambda_function",
+                        "function_version": "$LATEST",
+                        "invoked_function_arn": "arn:aws:lambda:...",
+                        "memory_limit_in_mb": "128",
+                        "aws_request_id": "some-unique-id",
+                        "log_group_name": "/aws/lambda/my_lambda_function",
+                        "log_stream_name": "2021/03/29/[$LATEST]abcdef1234567890"
+                    }
+
+
+        aws_event_serailized = json.dumps(aws_event).encode('utf-8')
+        aws_context_serailized = json.dumps(aws_context).encode('utf-8')
+
+        
+        entry_point_lambda(aws_event_serailized, aws_context_serailized)
+        out, err = capfd.readouterr()
+
+        # Split the output into lines and check each line
+        for line in out.split('\n'):
+            assert not line.startswith('Error'), f"Test failed due to error print statement {out}"
+
+        # Optionally, also check the standard error (stderr)
+        for line in err.split('\n'):
+            assert not line.startswith('Error'), f"Test failed due to error print statement {err}"
+
