24f0ad27382d96743b233715191e146182ce9eb1
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-01 15:11:17 -0500

    Update event_deploy.py

--- metrics/src/event_processor/events/event_deploy.py
+++ metrics/src/event_processor/events/event_deploy.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
-import pandas as df
+import pandas as pd
+import json
 
 from src.event_processor.events.event import Event
 from src.event_processor.calculations.dora_deploy_frequency_calculator import DORADeployFrequencyCalculator
 from src.event_processor.database.db_connection import DBConnection
 from src.event_processor.logger.event_logger import EventLogger
-
 
 class EventDeploy(Event):
 
@@ -15,26 +15,35 @@ class EventDeploy(Event):
                  logger: EventLogger, 
                  deploy_frequency_timewindow_days: int) -> None:
         super().__init__(payload, db_connection, logger)
-
         self.deploy_frequency_timewindow_days = deploy_frequency_timewindow_days
 
     def process(self) -> None:
+        self._write_event_to_db()
+        self._log_event()
+        deploy_frequency = self._calculate_deploy_frequency()
+        self._report_deploy_frequency(deploy_frequency)
 
+    def _write_event_to_db(self):
         self.db_connection.write_event_to_db(self.payload)
 
+    def _log_event(self):
         res = self.logger.get_event_log_item_from_df_event(self.payload)
         self.logger.send_event_to_logger(res)
 
-        daily_deploy_volume_df = self.db_connection.get_daily_deploy_volume(None)
-        dora_deploy_frequency_calculator = DORADeployFrequencyCalculator()
+    def _calculate_deploy_frequency(self) -> float:
         start_date = datetime.now() - timedelta(days=self.deploy_frequency_timewindow_days)
         end_date = datetime.now()
+        daily_deploy_volume_df = self.db_connection.get_daily_deploy_volume(None)
 
-        deploy_frequency = dora_deploy_frequency_calculator.get_deployment_frequency(daily_deploy_volume=daily_deploy_volume_df,
-                                                                                     start_date=start_date,
-                                                                                     end_date=end_date)
+        dora_deploy_frequency_calculator = DORADeployFrequencyCalculator()
+        return dora_deploy_frequency_calculator.get_deployment_frequency(
+            daily_deploy_volume=daily_deploy_volume_df,
+            start_date=start_date,
+            end_date=end_date
+        )
 
-        metadata = '{"deploy_frequency": ' + str(deploy_frequency) + '}'
+    def _report_deploy_frequency(self, deploy_frequency: float):
+        metadata = json.dumps({"deploy_frequency": deploy_frequency})
         event = {
             'Time': datetime.now(),
             'Repo': 'ALL',
@@ -42,8 +51,7 @@ class EventDeploy(Event):
             'Metadata': metadata
         }
         
-        event_df = df.DataFrame([event])
+        event_df = pd.DataFrame([event])
         self.db_connection.write_event_to_db(event_df)
-
         res = self.logger.get_event_log_item_from_df_event(event_df)
         self.logger.send_event_to_logger(res)
