70d4c517b2c7b58c5ad4272e3cd4dd4392304181
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-03 10:43:18 -0500

    moving aws from env file to param store

--- metrics/src/event_processor/params/config_aws.py
+++ metrics/src/event_processor/params/config_aws.py
@@ -1,6 +1,5 @@
 import os
 import json
-from dotenv import load_dotenv
 import boto3
 
 from src.event_processor.params.config import Config
@@ -9,7 +8,7 @@ from src.event_processor.params.config import Config
 class ConfigAWS(Config):
 
     def __init__(self):
-        self._load_environment_variables()
+        self._initialize_param_store_client()
         self._initialize_secrets_manager_client()
 
     def get_expected_auth_token(self) -> str:
@@ -21,31 +20,20 @@ class ConfigAWS(Config):
         return self._extract_value_from_secret(secret, 'LOGGER_API_KEY')
 
     def get_db_name(self) -> str:
-        db_name = os.getenv('DEVOPS_DB_NAME')
-        if not db_name:
-            raise ValueError("DEVOPS_DB_NAME environment variable is not set.")
+        db_name = self._fetch_parameter_from_param_store('DEVOPS_DB_NAME')
         return db_name
 
-    def get_db_table_name(self) -> str:
-        db_table_name = os.getenv('DEVOPS_TABLE_NAME')
-        if not db_table_name:
-            raise ValueError(
-                "DEVOPS_TABLE_NAME environment variable is not set.")
+    def get_db_table_name(self) -> str:  
+        db_table_name = self._fetch_parameter_from_param_store('DEVOPS_TABLE_NAME')
         return db_table_name
-
+        
     def get_deployment_freq_timeframe_days(self) -> int:
-        days = os.getenv('DEPLOYMENT_FREQ_TIMEFRAME_DAY')
-        if not days:
-            raise ValueError(
-                "DEPLOYMENT_FREQ_TIMEFRAME_DAY environment variable is not set.")
-
+        days = self._fetch_parameter_from_param_store('DEPLOYMENT_FREQ_TIMEFRAME_DAY')
         return int(days)
 
-    def _load_environment_variables(self):
-        self.secret_store_name = os.getenv('SECRET_STORE_NAME')
-        if not self.secret_store_name:
-            raise ValueError(
-                "SECRET_STORE_NAME environment variable is not set.")
+    def get_secret_store_name(self) -> str:
+        secret_store_name = self._fetch_parameter_from_param_store('SECRET_STORE_NAME')
+        return secret_store_name
 
     def _initialize_secrets_manager_client(self):
         try:
@@ -58,18 +46,33 @@ class ConfigAWS(Config):
                 f"Failed to initialize AWS Secrets Manager client: {str(e)}")
 
     def _fetch_secret(self):
-        try:
-            response = self.secrets_manager_client.get_secret_value(
-                SecretId=self.secret_store_name)
-        except Exception as e:
-            raise e
+        response = self.secrets_manager_client.get_secret_value(
+            SecretId=self.get_secret_store_name())
         if 'SecretString' not in response:
             raise ValueError("Secret token not found in Secrets Manager.")
 
         return response['SecretString']
-
     def _extract_value_from_secret(self, secret_str, key):
         secret_dict = json.loads(secret_str)
         if key not in secret_dict:
             raise ValueError(f"{key} not found in the secret.")
         return secret_dict[key]
+    
+    def _initialize_param_store_client(self):
+        try:
+            self.ssm_client = boto3.client('ssm')
+            if self.ssm_client is None:
+                raise ValueError(
+                    "Failed to initialize AWS SSM client.")
+        except Exception as e:
+            raise ValueError(
+                f"Failed to initialize AWS SSM client: {str(e)}")
+        
+    def _fetch_parameter_from_param_store(self, param_name):
+        try:
+            response = self.ssm_client.get_parameter(
+                Name=param_name, WithDecryption=True)
+            return response['Parameter']['Value']
+        except Exception as e:
+            raise ValueError(f"Failed to fetch parameter {param_name}: {str(e)}")
+
