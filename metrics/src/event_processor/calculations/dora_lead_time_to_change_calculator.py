8abcd06c91e2cf79e93c07d6cbea133282e903a8
tomerXarchimedes <tomer@archimedesfi.com>
2024-02-03 18:10:49 -0500

    Update dora_lead_time_to_change_calculator.py

--- metrics/src/event_processor/calculations/dora_lead_time_to_change_calculator.py
+++ metrics/src/event_processor/calculations/dora_lead_time_to_change_calculator.py
@@ -1,35 +1,48 @@
 import pandas as pd
 
 
-class DORALeadTimeToChangeCalculator():
+class DORALeadTimeToChangeCalculator:
 
-    def calculate_median_day_lead_time_for_deploy(self, push_events: list[pd.DataFrame], deploy_event_date: pd.Timestamp) -> float: 
-        lead_times = self._calculate_lead_times(push_events, deploy_event_date) 
-        res = self._calculate_median(lead_times)
-        return res
+    def calculate_median_day_lead_time_for_deploy(self, push_events: list[pd.DataFrame], deploy_event_date: pd.Timestamp) -> float:
+        lead_times = self._calculate_lead_times_for_all_pushes(push_events, deploy_event_date)
+        median = self._calculate_median(lead_times)
 
-    def _calculate_lead_times(self, push_events: list[pd.DataFrame], deploy_date: pd.Timestamp) -> list[int]:
+        return median
+
+    def _calculate_lead_times_for_all_pushes(self, push_events: list[pd.DataFrame], deploy_date: pd.Timestamp) -> list[int]:
         lead_times = []
         for event_df in push_events:
-            if not event_df.empty and 'Time' in event_df.columns:
-                push_date = pd.to_datetime(event_df['Time'].iloc[0])
+            if not self._is_valid_event(event_df):
+                continue
 
-                # Ensure push_date has the same timezone as deploy_date
-                if deploy_date.tz is not None:
-                    push_date = push_date.tz_localize(deploy_date.tz)
-
-                # If deploy_date is naive, make push_date naive as well
-                elif deploy_date.tz is None and push_date.tz is not None:
-                    push_date = push_date.tz_localize(None)
-
-                lead_time = (deploy_date - push_date).days
-                lead_times.append(lead_time)
+            lead_time = self._calculate_lead_time(event_df, deploy_date)
+            lead_times.append(lead_time)
 
         return lead_times
 
+    def _calculate_lead_time(self, event_df: pd.DataFrame, deploy_date: pd.Timestamp) -> int:
+        push_date = self._normalize_push_date(event_df, deploy_date)
+        return self._compute_lead_time(push_date, deploy_date)
 
-    @staticmethod
-    def _calculate_median(values: list[int]) -> float:
+    def _is_valid_event(self, event_df: pd.DataFrame) -> bool:
+        return not event_df.empty and 'Time' in event_df.columns
+
+    def _normalize_push_date(self, event_df: pd.DataFrame, deploy_date: pd.Timestamp) -> pd.Timestamp:
+        push_date = pd.to_datetime(event_df['Time'].iloc[0])
+        push_date = self._adjust_timezone(push_date, deploy_date)
+        return push_date
+
+    def _adjust_timezone(self, push_date: pd.Timestamp, deploy_date: pd.Timestamp) -> pd.Timestamp:
+        if deploy_date.tz is not None:
+            return push_date.tz_localize(deploy_date.tz, copy=True) 
+        elif deploy_date.tz is None and push_date.tz is not None:
+            return push_date.tz_localize(None, copy=True)
+        return push_date
+
+    def _compute_lead_time(self, push_date: pd.Timestamp, deploy_date: pd.Timestamp) -> int:
+        return (deploy_date - push_date).days
+
+    def _calculate_median(self, values: list[int]) -> float:
         if not values:
             return float('nan')
 
